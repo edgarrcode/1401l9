7351992e55a47671651d76a4e2cb085ff37c8125
Rodriguez <eerodriguez@utepc-144439.utep.edu>
2016-04-21 15:33:15 -0600

    t quit

---
+++ DNAsequence.java
@@ -0,0 +1,133 @@
+public class DNAsequence {
+
+	/**************** ATTRIBUTES *******************************************
+	/* Here go your attributes, i.e., the information that is contained in 
+	 * your new "type" 
+	 * We can also see these new types as "blue-prints" of "things" we are 
+	 * going to build 
+	 * Expected attributes are:
+	 * a.	A name: of type string
+     * b.	A DNA sequence: of type string
+     * c.	A length: of type int
+     * d.	A number of A’s: of type int
+     * e.	A number of C’s: of type int
+     * f.	A number of G’s: of type int
+     * g.	A number of T’s: of type int
+	 ***********************************************************************/
+	private String name;
+	private String sequence;
+	private int length;
+	private int As;
+    private int Cs;
+    private int Gs;
+    private int Ts;
+	
+    /***************** METHODS *********************************************
+     * Note that none of the methods below are static.
+     * It means that once you have defined a object of type DNAsequence, 
+     * let's say mySequence, then you will call these methods as:
+     * mySequence.printSequence(), or mySequence.setName("this new name"),
+     * etc.
+     ***********************************************************************/
+    
+	/**************** CONSTRUCTORS *****************************************
+	 * Note that the signatures are different from those we are used to 
+	 ***********************************************************************/
+	/* default constructor: provided to you. You should not touch the next two lines of code */
+	public DNAsequence() {
+	}
+	
+	public DNAsequence(String seqName, String seq, int seqlength) {
+        // your code goes here
+        name = seqName;
+        sequence = seq;
+        length = seqlength;
+    }
+	
+	/***************** SETTERS / MUTATORS **********************************
+	 * Methods that allow to set or modify the values of the attributes
+	 * One method per attribute
+	 * Note that the methods are not static (to be explained -- much -- later)
+	 ***********************************************************************/
+	/* one per attribute: we provide you with two, you have to provide the others */
+	public void setName(String seqName) {
+		name = seqName;
+	}
+	
+	public void setSequence(String seqSequence) {
+		sequence = seqSequence;
+	}
+	
+	public void setlength(int seqlength) {
+		// your code goes here
+		length = seqlength;
+	}
+	
+    // complete here with more mutators, as relevant
+    
+	/**************** GETTERS / ACCESSORS **********************************
+	 * Methods that allow to access the values of the attributes
+	 * One method per attribute
+	 * Note that the methods are not static (to be explained -- much -- later)
+	 ***********************************************************************/
+	/* one per attribute: we provide you with the one, you have to provide the others */
+	public String getName() {
+		return name;
+	}
+	
+	public String getSequence() {
+		return sequence;
+	}
+	
+	public int getLength() {
+		// your code goes here
+		return length;
+	}
+	
+    // complete here with more accessors, as relevant
+	
+	/***********************************************************************
+	 * Other methods, depending on needs 
+	 ***********************************************************************/
+
+    /***********************************************************************
+	 * Here we are asking you to design a method that prints the information
+	 * about any DNAsequence
+     * Method Print: this method prints the name of the sequence followed by 
+     * the sequence itself, then prints its length and the number of A’s, 
+     * C’s, G’s, and T’s.
+	 ***********************************************************************/
+	public void Print() {
+	   // your code goes here
+		System.out.println("Name: " + name);
+		System.out.println("Sequence: " + sequence);
+		System.out.println("Length: " + length);
+		System.out.println("As: " + As);
+		System.out.println("Cs: " + Cs);
+		System.out.println("Gs: " + Gs);
+		System.out.println("Ts: " + Ts);
+    }
+	
+    /***********************************************************************
+     * Method countSubStringMatch: 
+     * as defined in Lab 8, except for the parameters: this method only takes 
+     * one parameter, the target (the key is the object on which the method 
+     * is applied). Note: You are free to use your iterative or recursive method.  
+	 ***********************************************************************/
+    public int countSubStringMatch(String target) {
+        int result = 0;
+        // your code goes here
+        String myStr = "";
+
+        if(target.length() < sequence.length()) {
+            return 0;
+        }
+
+        myStr = target.substring(0, sequence.length());
+        if (sequence.equals(myStr)) {
+            result = result + 1;
+        }
+
+        return (result + countSubStringMatch(target.substring(1)));
+    }
+}
---
+++ DNAtools.java
@@ -0,0 +1,187 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DNAtools {
+    
+    /**********************************************************************************
+     * ReadSequencesFromFile: 
+     * This method takes the name of a file as parameter: 
+     * the given file contains information about DNA sequences (one sequence per 
+     * line, including name and sequence). 
+     * It reads the information and fills an array of DNAsequences (a 1D array of 
+     * elements of type DNAsequence) with this information. 
+     * It returns the array of DNAsequences. 
+     ***********************************************************************************/
+    public static DNAsequence[] ReadSequencesFromFile(String filename) throws IOException {
+        
+        FileReader fr = new FileReader(filename);
+        BufferedReader textReader = new BufferedReader(fr);
+        textReader.mark(1000); // here we assume that the file won't be longer than 1000 lines
+        
+        // here we count how many DNA sequences we have: the number of DNA sequences is the number of lines in the file
+        // because for each sequence there is one sequence information per line
+        int numOfLines = 0; //addedByEdgar
+        while (textReader.readLine()!=null) {
+            numOfLines++;
+        }
+        
+        DNAsequence[] myDNAsequences = new DNAsequence[numOfLines];
+
+        // your code goes here
+        textReader.reset();
+        String[] line = new String[1];
+        String seqName = "";
+        String seq = "";
+        for (int i = 0; i < numOfLines; i++) {
+            line = textReader.readLine().split(" ");
+            seqName = line[0];
+            seq = line[1];
+            myDNAsequences[i] = new DNAsequence(seqName,seq,seq.length());
+        }
+        
+        textReader.close();
+        return myDNAsequences; 
+    }
+    
+    /**********************************************************************************
+     * ReadTargetsFromFile: 
+     * This method takes the name of a file as parameter: 
+     * The given file contains information about DNA sequences (one sequence per 
+     * line, which is just one string per line – no name in this case). 
+     * It reads the information and fills an array of Strings with this information. 
+     * It returns this array. 
+     ***********************************************************************************/
+    public static String[] ReadTargetsFromFile(String filename) throws IOException {
+        
+        FileReader fr = new FileReader(filename);
+        BufferedReader textReader = new BufferedReader(fr);
+        textReader.mark(1000); // here we assume that the file won't be longer than 1000 lines
+        
+        // here we count how many DNA sequences we have: the number of DNA sequences is the number of lines in the file
+        // because for each sequence there is one sequence per line
+        int numOfLines = 0;
+        while (textReader.readLine()!=null) {
+            numOfLines++;
+        }
+        
+        String[] myTargets = new String[numOfLines];
+
+        // your code goes here
+        textReader.reset();
+        for (int i = 0; i < numOfLines; i++) {
+            myTargets[i] = textReader.readLine();
+        }
+        
+        textReader.close();
+        return myTargets; 
+    }
+
+
+    /**********************************************************************************
+     * PrintSequenceArray: 
+     * This method takes a 1D array of DNAsequences and prints it out (using the print 
+     * method from the DNAsequence class).
+     ***********************************************************************************/
+    public static void PrintSequenceArray(DNAsequence[] A) {
+        // your code goes here
+        for (int i = 0; i < A.length; i++) {
+            System.out.println("------");
+            A[i].Print();
+        }
+
+    }
+    
+    /**********************************************************************************
+     * FindBestMatchSequence: 
+     * This method takes a 1D array of DNAsequences and a 1D array of strings (the target 
+     * strings), and identifies the DNAsequence (within the given 1D array of DNAsequences) 
+     * whose average of the number of occurrences in each of the target strings is highest.
+     ***********************************************************************************/
+    public static DNAsequence FindBestMatchSequence(DNAsequence[] A, String[] targets) {
+        // your code goes here
+        int resultLargest = 0;
+        int resultTemp = 0;
+        DNAsequence sequenceLargest = A[0];
+        for (int i = 0; i < A.length; i++) {
+            resultTemp = A[i].countSubStringMatch(targets[i]);
+            if (resultTemp > resultLargest) {
+                resultLargest = resultTemp;
+                sequenceLargest = A[i];
+            }
+        }
+
+        return sequenceLargest;
+    }
+
+    /**********************************************************************************
+     * SortByBestOccurrenceAverage: 
+     * This method takes a 1D array of DNAsequences and a 1D array of strings (the 
+     * target strings), and sorts the 1D array of DNAsequences by the average of their 
+     * number of occurrences in each of the target strings (highest to lowest).
+     ***********************************************************************************/
+    //public static void SortByBestOccurrenceAverage(DNAsequence[] A, String[] targets) {
+        // your code goes here
+    //}
+
+
+    /**********************************************************************************
+     * SortByLetter: 
+     * This method takes a 1D array of DNAsequences and a letter (among A, C, G, or T) 
+     * as an input and sorts the 1D array of DNAsequences by the number of such letter 
+     * in each DNAsequence (in descending order of such letter). 
+     ***********************************************************************************/
+    /*public static void SortByLetter(DNAsequence[] A, char c) {
+        // your code goes here   
+    }*/
+    
+    
+    /**********************************************************************************
+     * The main method should go as follows:
+     * 1.    You ask the user for a file name
+     * 2.    You read the file and retrieve information about DNAsequences by calling method ReadSequencesFromFile 
+     * 3.    You ask the user for a file name
+     * 4.    You read the file and retrieve information about target strings by calling method ReadTargetsFromFile 
+     * 5.    You run the relevant class methods from DNAsequence to fill the attributes.
+     * 6.    You sort the array obtained in Step 2 using method SortByBestOccurrenceAverage and print it out. 
+     * 7.    You sort the array obtained in Step 6 using method SortByLetter where the letter is A, and print it out. 
+     * 8.    You sort the array obtained in Step 7 using method SortByLetter where the letter is C, and print it out. 
+     * 9.    You sort the array obtained in Step 8 using method SortByLetter where the letter is G, and print it out. 
+     * 10.    You sort the array obtained in Step 9 using method SortByLetter where the letter is T, and print it out. 
+     ***********************************************************************************/
+    public static void main(String[] args) throws IOException {
+        // your code goes here
+        Scanner scnr = new Scanner(System.in);
+        String filename = "";
+        int i = 0;
+
+        //* 1.  You ask the user for a file name
+        System.out.print("Enter file name with sequences:");
+        filename = scnr.next();
+        
+        //* 2.    You read the file and retrieve information about DNAsequences by calling method ReadSequencesFromFile 
+        DNAsequence[] myDNAsequences = ReadSequencesFromFile(filename);
+
+        //* 3.    You ask the user for a file name
+        System.out.print("Enter file name with targets:");
+        filename = scnr.next();
+
+        //* 4.    You read the file and retrieve information about target strings by calling method ReadTargetsFromFile 
+        String[] myTargets = ReadTargetsFromFile(filename);
+
+        //* 5.    You run the relevant class methods from DNAsequence to fill the attributes.
+        PrintSequenceArray(myDNAsequences);
+        System.out.println("------");
+        System.out.println("Setting myDNAsequences[0].name to \"Edgars\"");
+        myDNAsequences[0].setName("Edgars");
+        System.out.println("Setting myDNAsequences[0].sequence to \"EEEEE\"");
+        myDNAsequences[0].setSequence("EEEEE");
+        System.out.println("Setting myDNAsequences[0].length to \"5\"");
+        myDNAsequences[0].setlength(5);
+        PrintSequenceArray(myDNAsequences);
+
+        FindBestMatchSequence(myDNAsequences,myTargets);
+    }
+
+}
